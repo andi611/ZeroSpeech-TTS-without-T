84be77547d4b761f8c119a6b3ec1e733d0090f74
MutatedFlood <patrick1031wang@gmail.com>
2019-02-25 14:08:53 +0800

    autoencoder approach

--- discrete.py
+++ discrete.py
@@ -4,24 +4,84 @@ from os import path
 import numpy as np
 import torch
 from sklearn.cluster import KMeans
-from torch import cuda, nn, optim
+from torch import cuda, distributions, nn, optim
+from torch.nn import functional as F
 
 from model import Decoder
 from trainer import Trainer
 from utils import grad_clip, reset_grad
 
 
+class GumbelSoftmax:
+    def __init__(self, u=0, b=1, t=.1, dim=-1):
+        self.gumbel = distributions.Gumbel(loc=u, scale=b)
+        self.temperature = t
+        self.dim = dim
+
+    def __call__(self, inputs):
+        sample = self.gumbel.sample(inputs.shape)
+        x = torch.log(inputs)+sample
+        return F.softmax(x/self.temperature, dim=self.dim)
+
+
+class ClassEncoder:
+    def __init__(self, input_dim, n_classes, hidden_dim=100):
+        self.n_classes = n_classes
+        self.linear_block = nn.ModuleList([
+            nn.Linear(in_features=input_dim, out_features=hidden_dim),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_dim, out_features=n_classes)
+        ])
+        self.input_dim = input_dim
+        self.gumbel = GumbelSoftmax()
+
+    def forward(self, inputs):
+        inputs = inputs.view(-1, self.input_dim)
+        net = inputs
+        for layer in self.linear_block:
+            net = layer(net)
+        return self.gumbel(net)
+
+
+# class LatticeGumbel:
+#     def __init__(self, n_lattices):
+#         self.n_lattices = n_lattices
+#         self.gumbel_softmax = GumbelSoftmax()
+
+#     def __call__(self, inputs):
+#         return None
+
+#     def process(self, inputs, dim=-1):
+#         normalized = self.normalize(inputs, dim=dim)
+#         lattice = torch.floor(normalized * self.n_lattices).float()
+#         lattice = torch
+#         gumbel = self.gumbel_softmax(lattice)
+
+#     @staticmethod
+#     def normalize(inputs, dim=-1):
+#         maximum = inputs.max(dim)[0]
+#         minimum = inputs.min(dim)[0]
+#         difference = maximum-minimum
+#         return (inputs-minimum)/difference
+
+
 def clustering(inputs, n_clusters):
     class LookUp:
-        def __init__(self, k_means):
+        def __init__(self, k_means, shapes):
             self.k_means = k_means
+            self.shapes = shapes
 
-        def __call__(self, inputs):
-            cls = [self.k_means.predict(np.expand_dims(i, 0)) for i in inputs]
-            return np.array([self.k_means.cluster_centers_[c.squeeze()] for c in cls])
+        def __call__(self, inputs, device='cuda'if cuda.is_available() else 'cpu'):
+            inputs = np.array(inputs)
+            if inputs.shape == self.shapes[1:]:
+                inputs = np.expand_dims(inputs, 0)
+            cls = np.array([self.k_means.predict(i.reshape(1, -1))
+                            for i in inputs])
+            return torch.tensor(np.array([self.k_means.cluster_centers_[c].reshape(*self.shapes[1:]) for c in cls]), device=device)
     k_means = KMeans(n_clusters=n_clusters)
-    k_means.fit(inputs)
-    return k_means, LookUp(k_means)
+    reshaped = inputs.reshape(inputs.shape[0], -1)
+    k_means.fit(reshaped)
+    return k_means, LookUp(k_means, shapes=inputs.shape)
 
 
 def train_discrete_decoder(trainer, look_up, model_path, flag='train'):
