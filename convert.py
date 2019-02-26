e626ef3cae59a3c6050f808734dee6b053859b58
andi611 <tingweiandyliu@gmail.com>
2019-02-26 20:48:33 +0800

    update test_single()

--- convert.py
+++ convert.py
@@ -198,9 +198,10 @@ def test_single(trainer, seg_len, speaker2id_path, result_dir, enc_only, s_speak
 
 	sf.write(os.path.join(result_dir, 'result.wav'), wav_data, hp.sr, 'PCM_24')
 	with open(os.path.join(result_dir, 'result.txt'), 'w') as file:
-		for enc in encodings: 
+		for enc in encodings:
+			print(np.shape(enc))
 			for element in enc:
-				file.write(str(int(element)))
+				file.write(str(int(element)) + ' ')
 			file.write('\n')
 
 	print('Testing on source speaker {} and target speaker {}, output shape: {}'.format(s_speaker, t_speaker, wav_data.shape))
