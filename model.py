7fc7016d72a693f1ee51b57d0acaa41e9084c984
andi611 <tingweiandyliu@gmail.com>
2019-03-07 15:12:24 +0800

    fix decoder output to sigmoid

--- model.py
+++ model.py
@@ -302,7 +302,7 @@ class Decoder(nn.Module):
 		out = linear(out, self.dense5)
 		out = F.leaky_relu(out, negative_slope=self.ns)
 		out = linear(out, self.linear)
-		out = F.sigmoid(out)
+		out = torch.sigmoid(out)
 		return out
 
 
