89272e2835f9ab3b0ab861f05451bc37994bae57
andi611 <tingweiandyliu@gmail.com>
2019-03-29 23:11:02 +0800

    update

--- plotter.py
+++ plotter.py
@@ -15,7 +15,7 @@ import csv
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-plt.rcParams.update({'font.size': 17})
+plt.rcParams.update({'font.size': 20})
 
 ###########
 # LAMBDAS #
@@ -43,13 +43,30 @@ def get_config():
 ##################
 # PLOT TRADE OFF #
 ##################
-def plot_tradeoff(wer, br, name):
-	plt.figure(figsize=(9, 5))
+def plot_tradeoff(wer, br, dim, name):
+	fig = plt.figure(figsize=(12, 5))
+
 	plt.xlabel('Bit Rate')
-	plt.ylabel('WER')
+	plt.ylabel('CER')
 	plt.gca().invert_xaxis()
+	fig.autofmt_xdate()
+	plt.plot(br, wer, linestyle=':', marker='o', color='m') # Ours
+	plt.plot([71.98], [1.000], linestyle=':', marker='o', color='r') # Baseline
+	plt.plot([138.45, 138.45], [0.036, 0.040], linestyle=':', marker='o', color='b') # Continues
+	for x, y, i in zip(br, wer, dim):
+		plt.annotate(i, xy=(x-1, y), xycoords='data', xytext=(+25, -10) if i > 16 else (+13, -30),
+			textcoords='offset points', fontsize=20,
+			arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=0.5"))
+
+	plt.annotate('baseline', xy=(71.98-1, 1.000), xycoords='data', xytext=(-15, -30),
+			textcoords='offset points', fontsize=20,
+			arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=0.5"))
+
+	plt.annotate('continues', xy=(138.45-1, 0.038), xycoords='data', xytext=(+40, -5),
+			textcoords='offset points', fontsize=20,
+			arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=0.5"))
 	# plt.xscale('log')
-	plt.plot(to_str(br), wer, linestyle=':', marker='o', color='red')
+	plt.tight_layout()
 	plt.savefig(name)
 	plt.close()
 
@@ -61,7 +78,7 @@ def plot_encoding(wer, br, dim, name):
 	plt.figure(figsize=(9, 5))
 	plt.ylabel('Linear Interpolated WER and Bit Rate')
 	plt.xlabel('Embedding Size')
-	plt.plot(to_str(dim), norm(wer), linestyle=':', marker='o', color='m', label='wer')
+	plt.plot(to_str(dim), norm(wer), linestyle=':', marker='o', color='m', label='cer')
 	plt.plot(to_str(dim), norm(br), linestyle=':', marker='o', color='c', label='br')
 	plt.legend(loc='center right')
 	plt.tight_layout()
@@ -85,7 +102,7 @@ def main():
 	dim = [1024, 512, 256, 128, 64, 32, 16, 8, 7, 6, 5]
 
 	if args.all or args.tradeoff:
-		plot_tradeoff(wer, br, os.path.join(args.result_dir, 'tradeoff.png'))
+		plot_tradeoff(wer, br, dim, os.path.join(args.result_dir, 'tradeoff.png'))
 	if args.all or args.encoding:
 		plot_encoding(wer, br, dim, os.path.join(args.result_dir, 'encoding.png'))
 
